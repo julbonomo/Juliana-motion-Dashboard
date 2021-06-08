f022f8f7c9f340abc9fbe497e9da927178b1ec7e
julbonomo <63373173+julbonomo@users.noreply.github.com>
2021-06-08 11:21:14 -0400

    update
    6/8

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,173 +1,140 @@
 //IMPORTS
- import { gsap } from "gsap";
- import { GSDevTools } from "gsap/GSDevTools";
- import { MorphSVGPlugin} from "gsap/MorphSVGPlugin";
+import {gsap} from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import {MorphSVGPlugin} from "gsap/MorphSVGPlugin";
+// import {GSDevTools} from "gsap/GSDevTools";
  //import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 //register P//lugins
- gsap.registerPlugin(GSDevTools, MorphSVGPlugin);
+ gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
  //gsap.registerPlugin(MotionPathPlugin);
-// GSDevTools.create();
 
- //gsap.set("#moon",{transformOrigin:"center"});
+//  GSDevTools.create();
 
+ 
+MorphSVGPlugin.convertToPath("D, ten, twenty, thirty");
+ 
+const mainTL = gsap.timeline({paused:true});
 
+//  let mainTL = gsap.timeline({id:"main"});
 
- let ready = (callback)=>{
-        if(document.readyState != "loading")callback();
-        else document.addEventListener("DOMContentLoaded",callback);
-      }
-      
-      ready(() => {
-
-
- let mainTL = gsap.timeline({id:"main"});
-
-mainTL.from("#Gray-background", {scale:8, repeat:-1, duration:1}, "#Gray-background");
+// mainTL.from("#gray-background", {alpha:0, duration:5});
 
 
 
-function init(){
+
+
+// function init(){
 
 //Intro
 
-gsap.set(["#Big-rectangle", "#bottom-rectangle", "#top-rectangle", "#car", "#miles", "#P","#D",
-"#light-blur","#mph","#auto-drive","#light-function","#songs", "#sensor-bottomright",
-"#main-songline", "#sensor-topleft", "#white-songline", "#play-buttons"],{alpha:0});
 
-gsap.set("#Logo", { transform0rigin:"50% 50%"});
-gsap.set("#top-rectangle", { transform0rigin:"50% 50%"});
-gsap.set("#bottom-rectangle", { transform0rigin:"50% 50%"});
-gsap.set("#car", { transform0rigin:"50% 50%"});
-gsap.set("#battery", { transform0rigin:"50% 50%"});
-gsap.set("#miles", { transform0rigin:"50% 50%"});
-gsap.set("#P", { transform0rigin:"50% 50%"});
-gsap.set("auto-drive", { transform0rigin:"50% 50%"});
-gsap.set("auto-drive", { transform0rigin:"50% 50%"});
-gsap.set("auto-drive", { transform0rigin:"50% 50%"});
-
-
-
-
-
-}
-
+gsap.set("#logo", { transform0rigin:"50% 50%"});
 
 
 //Nested Timelines
 //   ***********  fadeInTL  ****************
-function fadeInTL(){
-      let tl = gsap.timeline();
-      tl.from("#Logo", {duration:2, scale:10, ease:"expo"}, "Logo")
-      .to("#Logo", {alpha:0, duration:2.5, ease:"back" }, "Logo")
+// function fadeInTL(){
+//       let mainTl = gsap.timeline();
+
+//logoFadeIn
+mainTL.from("#logo", {duration:3, scale:10, ease:"expo"})
+      .to("#logo", {alpha:0, duration:2.5, ease:"back" })
+
+//rectangle left to right 
+      .from ("#big-rectangle", {alpha:0})
+      .to ("#big-rectangle", {x:"-=0", duration:.25 })
+
+// //top rectangle left to right 
+      .from ("#top-rectangle", {alpha:0}, "toprectangle")
+      .to ("top-rectangle", {x:"-=100", duration:.25 })
+
+// //bottom rectangle left to right 
+      .from ("#bottom-rectangle", {alpha:0}, "bottomrectangle")
+      .to ("bottom-rectangle", {x:"-=100", duration:.25 })
+
+// //carFadeIn
+      .from("#car", {alpha:0, y:"+300", duration:1.5})
+
+     
+
+// //BatteryFadeIn
+      .from("#battery", {alpha:0, y:"-50"},"battery")
+      .from("#miles", {alpha:0, y:"-50"},"battery")
+  
+
+// //PFadeIn
+      .from("#P", {alpha:0})
+
+
+// //AutodriveFadeIn - lightfadein
+      .from("#auto-drive", {alpha:0, rotation:"-90", transformOrigin:"50% 50%", duration:2},"autodrive")
+      .from("#light-function", {alpha:0, duration:2},"autodrive")
+
+
+//lightsON
+      .from("#light-blur", {scale:0, transformOrigin:"50% 50%"})
+
+
+//songsFadeIn
+      .from ("#songs", {alpha:0, x:"-50"}, "songs")
+
+
+// //PMORPH to D
+
+      .to("#P",{duration:.5, morphSVG:"#D"})
       
-      .from ("#Big-rectangle", {alpha:0}, "Big-rectangle")
-      .to ("Big-rectangle", {x:"-=200", duration:1 })
 
-      .from ("#top-rectangle", {alpha:0}, "top-rectangle")
-      .to ("top-rectangle", {x:"-=100", duration:1 })
+      .from("#mph",{alpha:0},"mph")
+//tenFadeInFadeOut
+      .from("#ten",{alpha:0},"mph")
+      .to("#ten", {alpha:0, duration:2, ease:"back"})
 
-      .from ("#bottom-rectangle", {alpha:0}, "bottom-rectangle")
-      .to ("bottom-rectangle", {x:"-=100", duration:1 })
+//twentyFadeinout
+      .from("#twenty",{alpha:0})
+      .to("#twenty", {alpha:0, duration:2, ease:"back"})
 
+//thirtyFadein
+      .from("#thirty",{alpha:0})
+// .to("#thirty", {alpha:0, duration:5})
 
-      .from("#car", {duration:2, scale:2, ease:"expo"}, "car")
-      .to("#car", {alpha:0, duration:2.5, ease:"back" }, "car")
-
-      .from ("#battery", {alpha:0}, "battery")
-      .to ("battery", {x:"+=200", duration:2 })
-
-      .from ("#miles", {alpha:0}, "miles")
-      .to ("miles", {x:"+=200", duration:2 })
-
-      .from("#P", {duration:3.5, scale:.50, ease:"expo"}, "P")
-      .to("#P", {alpha:0, duration:2.5, ease:"expo" }, "P")
-
-      .from("#auto-drive", {duration:4, scale:.10, ease:"bounce"}, "auto-drive")
-      .to("#auto-drive", {alpha:0, duration:2.5, ease:"bounce" }, "auto-drive")
-
-      .from("#light-function", {duration:4, scale:.10, ease:"bounce"}, "light-function")
-      .to("#light-function", {alpha:0, duration:2.5, ease:"bounce" }, "light-function")
+      // .from("#twenty",{alpha:0},"mph")
+      // .to("#ten",{duration:.5, morphSVG:"#twenty"}, "logomorph")
+      // .to("#ten",{duration:.5, morphSVG:"#thirty"})
 
 
-//P to D
-      .from ("#P", {alpha:0})
-      .to ("#P", {duration:1.5, morphSVG: "#D"}, "+=1")
+//SongLine - playbuttons
+      .from("#main-songline", {alpha:0}, "song")
+      .from("#play-buttons", {alpha:0}, "song")
 
 
+//sensors
+      .from("#sensor-topleft", {alpha:0, rotation:"-20", transformOrigin:"50% 50%", duration:4},"sensors")
+      .from("#sensor-bottomright", {alpha:0, rotation:"+20", transformOrigin:"50% 50%", duration:4},"sensors")
+
+
+      // .from("#sensor-topleft",{duration:5, drawSVG:0},"-=.25","sensordraw") 
+      // .from("#sensor-bottomright",{duration:5, drawSVG:0},"-=.25","sensordraw") 
+
+//secondSongline
+      .from("#white-songline", {alpha:0, duration:5})   
+
+
+
+//       ;
+//       // return mainTl;
       
-      
-      ;
-      return tl;
-      
-}
+// // }
 
 
+// //  init();
 
+// // //   //2. show content - prevents FOUC
+// // gsap.set('#svg-container',{visibility:"visible"});
 
+// // //   //3. BUILD Main timeline
+// // mainTL.add(fadeInTL())
 
+mainTL.play();
 
-
-
-
-
-      // .to("#moon", {rotate:30, repeat:-1, yoyo:true, duration:5}, "sky")
-     // .to("#glow", {alpha:0, repeat:-1, yoyo:true, duration:2.5}, "sky")
-     // .from("#spaceship", {y:"+=400", duration:2}, "fly")
-     // .from("#spaceshipsmoke", {y:"+=400", duration:2}, "fly")
-     // .from("#smoke1", {y:"+=200", scale:0, duration:0.25})
-     // .from("#smoke2", {y:"+=200", scale:0, duration:0.25}, "-=0.5")
-     // .from("#smoke3", {y:"+=450", scale:0, duration:0.25}, "-=0.75")
-      // .to("#spaceshipsmoke", {y:"+=400", alpha:0, duration:0.75})
-      // .to("#smoke1", {y:"+=200", alpha:0, duration:0.75})
-      // .to("#smoke2", {y:"+=200", alpha:0, duration:0.75})
-      // .to("#smoke3", {y:"+=450", alpha:0, duration:0.75})
-      // .to("#spaceship", {duration: 5, motionPath: {path: "#spaceshippath", align:"#spaceshippath", alignOrigin: [0.5, 0.5], autoRotate: 90}, ease: "none", scale:0.5}, "-=3");
-
-
-
-//     //***********  fadeInTL init ****************
-
-//     //*********** zoomTL init ****************
-
-//     //*********** spaceshipTL init ****************
-
-//     //*********** liftOffTL init ****************
-
-//     //*********** flightTL init ****************
-
-//     //*********** moonLandingTL init ****************
-
-
-//   }
-
-//   //Nested Timelines
-//   //***********  fadeInTL  ****************
-//   function fadeInTL(){
-
-//   }
-
-//   //*********** zoomTL ****************
-
-//   //*********** spaceshipTL ****************
-
-//   //*********** liftOffTL ****************
-
-//   //*********** flightTL ****************
-
-//   //*********** moonLandingTL ****************
-
-
-//   //1. set initial properties
- init();
-
-//   //2. show content - prevents FOUC
-gsap.set('#svg-container',{visibility:"visible"});
-
-//   //3. BUILD Main timeline
-mainTL.add(fadeInTL())
-
-
-;//tl END
-
-      });
+// // ;
